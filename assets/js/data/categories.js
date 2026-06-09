d6ee9966f4ef0d74ab921afe830283ebe044ce73
Vipin Khatri <89641393+khatriji2510@users.noreply.github.com>
2026-06-10 00:06:43 +0530

    Update categories.js

--- assets/js/data/categories.js
+++ assets/js/data/categories.js
@@ -1,1 +1,12 @@
-export const categories = ['All'];
+// Edit this file to add, remove, or rename categories.
+// The shop filter will update automatically.
+
+export const categories = [
+  "All",
+  "Oversized Tees",
+  "Graphic Tees",
+  "Hoodies",
+  "New Drops",
+  "Best Sellers",
+  "Limited"
+];
