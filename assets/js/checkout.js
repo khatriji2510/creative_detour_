c576590ae9d804d5336c116a6441bed47f9cfafd
Vipin Khatri <89641393+khatriji2510@users.noreply.github.com>
2026-06-25 01:07:05 +0530

    Update checkout.js

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,1 +1,43 @@
+import { products } from "./data/products.js";
 
+const params = new URLSearchParams(
+  window.location.search
+);
+
+const productId =
+  params.get("id");
+
+const size =
+  params.get("size");
+
+const color =
+  params.get("color");
+
+const product =
+  products.find(
+    item => item.id === productId
+  );
+
+if (!product) {
+
+  document.body.innerHTML =
+    "<h1>Product not found</h1>";
+
+  throw new Error(
+    "Product not found"
+  );
+}
+
+document.getElementById(
+  "checkoutTitle"
+).textContent =
+  product.title;
+
+document.getElementById(
+  "checkoutSummary"
+).innerHTML = `
+  <p><strong>Size:</strong> ${size}</p>
+  <p><strong>Color:</strong> ${color}</p>
+  <p><strong>Product:</strong> ₹${product.price}</p>
+  <p><strong>Shipping:</strong> ₹${product.shipping}</p>
+`;
