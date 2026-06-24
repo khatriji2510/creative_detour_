457ac0a440033f77f75cfc56b6914760212ae37d
Vipin Khatri <89641393+khatriji2510@users.noreply.github.com>
2026-06-25 02:11:23 +0530

    Update verify-payment.js

--- api/verify-payment.js
+++ api/verify-payment.js
@@ -1,1 +1,44 @@
+const crypto = require("crypto");
 
+module.exports = async (req, res) => {
+
+  try {
+
+    const {
+      razorpay_order_id,
+      razorpay_payment_id,
+      razorpay_signature
+    } = req.body;
+
+    const generatedSignature =
+      crypto
+        .createHmac(
+          "sha256",
+          process.env.RAZORPAY_KEY_SECRET
+        )
+        .update(
+          razorpay_order_id +
+          "|" +
+          razorpay_payment_id
+        )
+        .digest("hex");
+
+    const isValid =
+      generatedSignature ===
+      razorpay_signature;
+
+    return res.status(200).json({
+      success: isValid
+    });
+
+  } catch (error) {
+
+    console.error(error);
+
+    return res.status(500).json({
+      success: false
+    });
+
+  }
+
+};
