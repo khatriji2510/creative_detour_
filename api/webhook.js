45af709334c285b32d5f8e4d5d09bf10fdac7dfb
Vipin Khatri <89641393+khatriji2510@users.noreply.github.com>
2026-06-25 15:19:50 +0530

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,1 +1,60 @@
+const crypto = require("crypto");
 
+module.exports = async (req, res) => {
+
+  try {
+
+    const signature =
+      req.headers["x-razorpay-signature"];
+
+    const body =
+      JSON.stringify(req.body);
+
+    const expectedSignature =
+      crypto
+        .createHmac(
+          "sha256",
+          process.env.WEBHOOK_SECRET
+        )
+        .update(body)
+        .digest("hex");
+
+    if (signature !== expectedSignature) {
+
+      return res
+        .status(400)
+        .json({
+          success: false,
+          message: "Invalid Signature"
+        });
+
+    }
+
+    console.log(
+      "Webhook Event:",
+      req.body.event
+    );
+
+    console.log(
+      req.body.payload
+    );
+
+    return res
+      .status(200)
+      .json({
+        success: true
+      });
+
+  } catch (error) {
+
+    console.error(error);
+
+    return res
+      .status(500)
+      .json({
+        success: false
+      });
+
+  }
+
+};
