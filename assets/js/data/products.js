2cc89d68325c566d62de36e683e79dfef97773b7
Vipin Khatri <89641393+khatriji2510@users.noreply.github.com>
2026-06-26 16:34:46 +0530

    Update products.js

--- assets/js/data/products.js
+++ assets/js/data/products.js
@@ -606,7 +606,51 @@ export const products = [
 
   sizeChart:
     "./assets/media/size-charts/oversizedtee-chart.png"
-}
+},
+
+  {
+  id: "blueberry-pancakes-regular-tee",
+
+  title: "Blueberry Pancakes Regular Tee",
+
+  price: 599,
+
+  shipping: 60,
+
+  codCharge: 50,
+
+  codAdvance: 199,
+
+  categories: ["Regular Tees"],
+
+  badge: "",
+
+  media: {
+    type: "image",
+    src: "./assets/media/products/blueberry-pancakes-regular-tee.png",
+    alt: "Blueberry Pancakes Regular Tee"
+  },
+
+  description:
+    "Premium 180 GSM regular fit graphic tee featuring original Creative Detour artwork.",
+
+  sizes: [
+    "XS",
+    "S",
+    "M",
+    "L",
+    "XL",
+    "XXL"
+  ],
+
+  colors: [
+    "White",
+    "Black"
+  ],
+
+  sizeChart:
+    "./assets/media/size-charts/regulartee-chart.png"
+},
 
   
   
